e08a8bec5c1b8e4694293158ab21e3cc0b10f095
Jeremy Bass <jeremybass@cableone.net>
2016-07-20 15:37:17 -0700

    account for pages not yet converted over

--- js/child_controll.js
+++ js/child_controll.js
@@ -3,5 +3,14 @@
 	if ($.browser.msie && $.browser.version == 9) {
 		$('html').addClass('ie9');
 	}
+
+	if( $('.page.type-page #content_area').length <= 0 ){
+		$('.page.type-page').addClass("content_less");
+	}
+
+
 	});
+
+
+
 }(jQuery));
