fad9e05a266877c3107099fdf24fc2ff4c8d25a0
Jeremy Bass <jeremybass@cableone.net>
2016-08-03 13:13:47 -0700

    make sure we have a widget picker that will be in tinymce

--- includes/assets/tinymce.js
+++ includes/assets/tinymce.js
@@ -1,30 +1,40 @@
 (function($) {
-	if( typeof widgetShortcode === 'undefined' )
+
+	'use strict';
+
+	// only do this if TinyMCE is available
+	if ( 'undefined' === typeof( tinymce ) ) {
 		return;
-
-	if( widgetShortcode.widgets.length < 1 )
+	}
+	// only do this if TinyMCE is available
+	if ( 'undefined' === typeof( widgetShortcode ) ) {
 		return;
+	}
 
-	tinymce.PluginManager.add( 'widgetShortcode', function( editor, url ) {
-		var items = [];
-		$.each( widgetShortcode.widgets, function( i, v ){
-			var item = {
-				'text' : v.title,
-				'body': {
-					'type': v.title
-				},
-				'onclick' : function(){
-					editor.insertContent( '[widget id="' + v.id + '"]' );
-				}
-			};
-			items.push( item );
-		} );
+	tinymce.create( 'tinymce.plugins.widgetShortcode', {
+		init: function( editor, url ) {
+			var items = [];
+			$.each( widgetShortcode.widgets, function( i, v ){
+				var item = {
+					'text' : v.title,
+					'body': {
+						'type': v.title
+					},
+					'onclick' : function(){
+						editor.insertContent( '[widget id="' + v.id + '"]' );
+					}
+				};
+				items.push( item );
+			} );
 
-		editor.addButton( 'widgetShortcode', {
-			title: widgetShortcode.title,
-			type : 'menubutton',
-			image : widgetShortcode.image,
-			menu : items
-		});
+			editor.addButton( 'widgetShortcode', {
+				title: widgetShortcode.title,
+				type : 'menubutton',
+				//cmd: 'widgetShortcode_insert',
+				image: widgetShortcode.image,
+				menu : items
+			} );
+		}
 	});
+	tinymce.PluginManager.add( 'widgetShortcode', tinymce.plugins.widgetShortcode );
 })(jQuery);
