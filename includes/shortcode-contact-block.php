89c64d745daa4c277d2bdf2357837d425f73c70d
Jeremy Bass <jeremybass@cableone.net>
2016-09-15 08:42:12 -0700

    alter the contect block to match request of by column display

--- includes/shortcode-contact-block.php
+++ includes/shortcode-contact-block.php
@@ -13,75 +13,78 @@ function contact_block_shortcode( $atts ) {
 	ob_start(); ?>
 
 
+	<div class="flex-row">
+		<div class="fourths-2 full-width-at-600 contact-us">
+			<h3><?php esc_attr_e( 'Address' )?>:</h3>
+			<div class="contact_name"><?php echo esc_attr( spine_get_option( 'contact_name' ) ); ?></div>
+			<div class="contact_department"><?php echo esc_attr( spine_get_option( 'contact_department' ) ); ?></div>
+			<div class="contact_streetAddress"><?php echo esc_attr( spine_get_option( 'contact_streetAddress' ) ); ?></div>
+			<?php
+			$street_address2 = fais_spine_get_option( 'contact_streetAddress2' );
+			if ( ! empty( $street_address2 ) ) {
+				?>
+				<div class="contact_streetAddress2"><?php echo esc_attr( $street_address2 ); ?></div>
+				<?php
+			}
+			?>
+			<div class="flex-row full-width items-start">
+				<div class="grid-part pad-no fifths-2 contact_addressLocality"><?php echo esc_attr( spine_get_option( 'contact_addressLocality' ) ); ?></div>
+				<div class="grid-part pad-no fifths-3 contact_postalCode"><?php echo esc_attr( spine_get_option( 'contact_postalCode' ) ); ?></div>
+			</div>
 
+			<?php
+			$verbal_location = fais_spine_get_option( 'contact_verbal_location' );
+			if ( ! empty( $verbal_location ) ) {
+				?><br/>
+					<h3 class="contact_verbal_location_title"><?php esc_attr_e( 'Verbal Location' )?>:</h3>
+					<div class="contact_verbal_location"><?php echo esc_attr( $verbal_location ); ?></div>
 
-<div class="flex-column items-start contact-us">
-	<h3><?php esc_attr_e( 'Address' )?>:</h3>
-	<div class="contact_name"><?php echo esc_attr( spine_get_option( 'contact_name' ) ); ?></div>
-	<div class="contact_department"><?php echo esc_attr( spine_get_option( 'contact_department' ) ); ?></div>
-	<div class="contact_streetAddress"><?php echo esc_attr( spine_get_option( 'contact_streetAddress' ) ); ?></div>
-	<?php
-	$street_address2 = fais_spine_get_option( 'contact_streetAddress2' );
-	if ( ! empty( $street_address2 ) ) {
-		?>
-		<div class="contact_streetAddress2"><?php echo esc_attr( $street_address2 ); ?></div>
-		<?php
-	}
-	?>
-    <div class="flex-row full-width items-start">
-		<div class="grid-part pad-no fifths-2 contact_addressLocality"><?php echo esc_attr( spine_get_option( 'contact_addressLocality' ) ); ?></div>
-		<div class="grid-part pad-no fifths-3 contact_postalCode"><?php echo esc_attr( spine_get_option( 'contact_postalCode' ) ); ?></div>
-    </div>
+				<?php
+			}
+			?>
+		</div>
+		<div class="fourths-2 full-width-at-600 contact-us">
 
-	<?php
-	$verbal_location = fais_spine_get_option( 'contact_verbal_location' );
-	if ( ! empty( $verbal_location ) ) {
-		?><br/>
-			<h3 class="contact_verbal_location_title"><?php esc_attr_e( 'Verbal Location' )?>:</h3>
-			<div class="contact_verbal_location"><?php echo esc_attr( $verbal_location ); ?></div>
+			<h3><?php esc_attr_e( 'Contact Methods' )?>:</h3>
+			<?php
+			$contact_telephone = spine_get_option( 'contact_telephone' );
+			if ( ! empty( $contact_telephone ) && '' !== trim( $contact_telephone ) ) {
+				?>
+				<div class="flex-row full-width items-start contact_telephone">
+					<h4 class="grid-part pad-no fifths-2"><?php esc_attr_e( 'Phone' )?>:</h4>
+					<div class="grid-part pad-no fifths-3"><?php echo esc_attr( $contact_telephone ); ?></div>
+				</div>
+				<?php
+			}
+			?>
 
-		<?php
-	}
-	?>
-    <br/>
-	<h3><?php esc_attr_e( 'Contact Methods' )?>:</h3>
+			<?php
+			$contact_email = spine_get_option( 'contact_email' );
+			if ( ! empty( $contact_email ) && '' !== trim( $contact_email ) ) {
+				?>
+				<div class="flex-row full-width items-start contact_email">
+					<h4 class="grid-part pad-no fifths-2"><?php esc_attr_e( 'Email' )?>:</h4>
+					<div class="grid-part pad-no fifths-3"><?php echo esc_attr( $contact_email ); ?></div>
+				</div>
+				<?php
+			}
+			?>
 
-	<?php
-	$contact_telephone = spine_get_option( 'contact_telephone' );
-	if ( ! empty( $contact_telephone ) && '' !== trim( $contact_telephone ) ) {
-		?>
-        <div class="flex-row full-width items-start contact_telephone">
-			<h4 class="grid-part pad-no fifths-2"><?php esc_attr_e( 'Phone' )?>:</h4>
-			<div class="grid-part pad-no fifths-3"><?php echo esc_attr( $contact_telephone ); ?></div>
-        </div>
-		<?php
-	}
-	?>
+			<?php
+			$contact_point = spine_get_option( 'contact_ContactPoint' );
+			if ( ! empty( $contact_point ) && '' !== trim( $contact_point ) ) {
+				?>
+				<div class="flex-row full-width items-start contact_point">
+					<h4 class="grid-part pad-no fifths-2"><?php esc_attr_e( 'Point of Contact' )?>:</h4>
+					<div class="grid-part pad-no fifths-3"><?php echo esc_attr( $contact_point ); ?></div>
+				</div>
+				<?php
+			}
+			?>
 
-	<?php
-	$contact_email = spine_get_option( 'contact_email' );
-	if ( ! empty( $contact_email ) && '' !== trim( $contact_email ) ) {
-		?>
-        <div class="flex-row full-width items-start contact_email">
-			<h4 class="grid-part pad-no fifths-2"><?php esc_attr_e( 'Email' )?>:</h4>
-			<div class="grid-part pad-no fifths-3"><?php echo esc_attr( $contact_email ); ?></div>
-        </div>
-		<?php
-	}
-	?>
+		</div>
 
-	<?php
-	$contact_point = spine_get_option( 'contact_ContactPoint' );
-	if ( ! empty( $contact_point ) && '' !== trim( $contact_point ) ) {
-		?>
-        <div class="flex-row full-width items-start contact_point">
-			<h4 class="grid-part pad-no fifths-2"><?php esc_attr_e( 'Point of Contact' )?>:</h4>
-			<div class="grid-part pad-no fifths-3"><?php echo esc_attr( $contact_point ); ?></div>
-        </div>
-		<?php
-	}
-	?>
-</div>
+	</div>
 
 <?php
 	$contact_content = ob_get_clean();
