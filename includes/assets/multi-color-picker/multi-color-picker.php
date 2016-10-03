8718304a32fc51fc20592ecfc2b10450c175fd0d
Jeremy Bass <jeremybass@cableone.net>
2016-10-03 12:25:09 -0700

    use a alpha color picker for the theme colors

---
+++ includes/assets/multi-color-picker/multi-color-picker.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * Multi Color Picker Customizer Control
+ *
+ * This control allows registering a group of color settings to a single control,
+ * and it uses a custom version of the stock WP color picker that supports RGBa
+ * color values and includes an opacity slider.
+ *
+ * This Multi Color Picker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this Multi Color Picker. If not, see <http://www.gnu.org/licenses/>.
+ */
+class Customize_Multi_Color_Control extends WP_Customize_Control {
+
+	/**
+	 * Official control name.
+	 */
+	public $type = 'multi-color';
+
+	/**
+	 * The array of color data.
+	 */
+	public $color_data;
+
+	/**
+	 * The label for this control group.
+	 */
+	public $label;
+
+	/**
+	 * Enqueue scripts and styles.
+	 *
+	 * Ideally these would get registered and given proper paths before this control object
+	 * gets initialized, then we could simply enqueue them here, but for completeness as a
+	 * stand alone class we'll register and enqueue them here.
+	 */
+	public function enqueue() {
+		wp_enqueue_script( 'multi-color-picker', get_stylesheet_directory_uri() . '/includes/assets/multi-color-picker/multi-color-picker.js', array( 'jquery', 'wp-color-picker' ), '1.0.0', true );
+		wp_enqueue_style( 'multi-color-picker', get_stylesheet_directory_uri() . '/includes/assets/multi-color-picker/multi-color-picker.css', array( 'wp-color-picker' ), '1.0.0' );
+	}
+
+	/**
+	 * Render the control.
+	 */
+	public function render_content() {
+
+		if ( isset( $this->label ) && '' !== $this->label ) {
+			echo '<span class="customize-control-title">' . sanitize_text_field( $this->label ) . '</span>';
+		}
+
+		if ( isset( $this->description ) && '' !== $this->description ) {
+			echo '<span class="description customize-control-description">' . sanitize_text_field( $this->description ) . '</span>';
+		}
+
+		// Output the div that will wrap our picker triggers.
+		echo '<style>
+
+.customize-control-multi-color {
+    position: relative;
+}
+.wp-picker-container.open {
+    position: absolute;
+    top: 0;
+    left: -10px;
+    background: #dadada;
+    padding: 5px 10px;
+    border: 1px solid #696969;
+}
+.wp-picker-container.open .close:before {
+    content: "\f00d";
+    width: 46px;
+    height: 17px;
+    z-index: 9999;
+    position: absolute;
+    top: 0;
+    right: 0px;
+}
+.wp-picker-container.open .close {
+    background: #696969;
+    border: 1px solid #696969;
+    display: inline-block;
+    font: normal normal normal 14px/1 FontAwesome;
+    font-size: inherit;
+    text-rendering: auto;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    width: 46px;
+    height: 17px;
+    z-index: 9999;
+    position: absolute;
+    top: 0;
+    right: 0px;
+    display: block;
+    color: #ddd;
+    text-align: center;
+    margin: 2px;
+    padding-top: 3px;
+    cursor: pointer;
+}
+ul.grouplist li.multi-color-picker-trigger {
+    float: none;
+    display: block;
+    width: 100%;
+    background: none;
+    box-shadow: none;
+}
+ul.grouplist li.multi-color-picker-trigger a.wp-color-result {
+    width: 12px;
+    box-shadow: 0 0 5px rgba(0,0,0,0.4) inset;
+    margin-right: 9px;
+}
+
+.multi-color-picker-triggers ul.grouplist li.multi-color-picker-trigger:hover {
+    border: 1px solid #868686;
+}
+.multi-color-picker-triggers ul.grouplist li.multi-color-picker-trigger {
+    border: 1px solid #000;
+    color: #555;
+    border-color: #ccc;
+    background: #f7f7f7;
+    -webkit-box-shadow: 0 1px 0 #ccc;
+    box-shadow: 0 1px 0 #ccc;
+    vertical-align: top;
+    line-height: 24px;
+    cursor:pointer;
+}
+
+
+</style><div class="multi-color-picker-triggers"></div>';
+
+		// Output the div that will wrap our picker triggers.
+		//echo '<ul class="color_group">';
+
+		// Loop over the color settings to output the actual inputs.
+		foreach ( $this->settings as $key => $setting ) {
+
+			// Store color data associated with the current setting.
+			$color_data = $this->color_data[ $setting->id ];
+
+			// Process the palette. Default to 'true' if no palette was passed in.
+			$palette = ( isset( $color_data['palette'] ) ) ? $color_data['palette'] : true;
+			if ( is_array( $palette ) ) {
+				$palette = implode( '|', $palette );
+			} elseif ( true === $palette || 'true' === $palette ) {
+				$palette = 'true';
+			} else {
+				$palette = 'false';
+			}
+
+			// Grab the rest of the setting data. Define defaults to make everything except 'label' optional.
+			$label = $color_data['label'];
+			$default = ( isset( $color_data['default'] ) ) ? $color_data['default'] : '#000000';
+			$show_opacity = ( isset( $color_data['show_opacity'] ) ) ? $color_data['show_opacity'] : 'true';
+
+			?>
+			<input type="text" class="multi-color-control" data-show-opacity="<?php echo esc_attr( $show_opacity ); ?>" data-palette="<?php echo $palette ?>" data-title="<?php echo esc_attr( $label ); ?>" data-default-color="<?php echo esc_attr( $default ); ?>" value="<?php echo $this->settings[ $key ]->value(); ?>" <?php echo $this->get_link( $key ); ?> />
+			<?php
+		}
+// Output the div that will wrap our picker triggers.
+		//echo '</ul>';
+	}
+}
--- includes/theme-customizer.php
+++ includes/theme-customizer.php
@@ -7,12 +7,27 @@ class Fais_Spine_Theme_Customizer {
 		add_action( 'customize_register', array( $this, 'customize_register' ) );
 	}
 
+
+
+
+
+
+
+
+
+
 	/**
 	 * Add custom settings and controls to the WP Customizer.
 	 *
 	 * @param WP_Customize_Manager $wp_customize
 	 */
 	public function customize_register( $wp_customize ) {
+
+	// Inlcude the Alpha Color Picker.
+	//require_once( dirname( __FILE__ ) . '/assets/alpha-color-picker/alpha-color-picker.php' );
+
+	// Include the Multi Color Picker.
+	require_once( dirname( __FILE__ ) . '/assets/multi-color-picker/multi-color-picker.php' );
 
 		// background image
 		$wp_customize->add_setting( 'spine_options[background_url]', array(
@@ -50,21 +65,115 @@ class Fais_Spine_Theme_Customizer {
 				)
 			)
 		);
+
+	/**
+	 * Define a default palette that we'll use for some of the colors.
+	 *
+	 * We could certainly define a separate palette for each color also.
+	 */
+	$palette = array(
+		'rgba(255, 0, 0, 0.7)',
+		'rgba(54, 0, 170, 0.8)',
+		'#FFCC00',
+		'rgba( 20, 20, 20, 0.8 )',
+		'#00CC77',
+	);
+
+	/**
+	 * Define our color settings under the group "Background Colors".
+	 *
+	 * This is one of the arrays that we'll pass to our helper function to
+	 * register each setting and group them under a single control.
+	 */
+	$bg_colors = array(
+		'background_color' => array(
+			'label'   => __( 'Body Background' ),
+			'default' => 'rgba(255, 0, 0, 0.7)',
+			'palette' => $palette, // This could also be true or false
+		),
+		'primary_accent_color' => array(
+			'label'      => __( 'Primary Accent Color' ),
+			'palette' => $palette, // This could also be true or false
+			'description' => 'The primary color will alter areas like the top borders. The classes to use:<br/><b>Background</b> : <code>primary-accent-bk</code> <br/><b>Text</b> : <code>primary-accent-text</code>',
+
+		),
+
+		'secoundary_accent_color' => array(
+			'label'      => __( 'Secoundary Accent Color' ),
+
+			'palette' => $palette, // This could also be true or false
+			'description' => 'The primary color will alter areas like the top borders. The classes to use:<br/><b>Background</b> : <code>secoundary-accent-bk</code> <br/><b>Text</b> : <code>secoundary-accent-text</code>',
+		),
+		'header_color' => array(
+			'label'      => __( 'Page Header Color' ),
+			'palette' => $palette, // This could also be true or false
+			'description' => '',
+		),
+		'header_text_color' => array(
+			'label'      => __( 'Header Text Color' ),
+			'palette' => $palette, // This could also be true or false'description' => 'The primary color will alter areas like the top borders. The classes to use:<br/><b>Background</b> : <code>secoundary-accent-bk</code> <br/><b>Text</b> : <code>secoundary-accent-text</code>',
+		),
+		/*'header_bg' => array(
+			'label'   => __( 'Header Background'),
+			'default' => 'rgba(54, 0, 170, 0.8)',
+			'palette' => $palette,
+		),
+		'sidebar_bg' => array(
+			'label'   => __( 'Sidebar Background' ),
+			'default' => '#FFCC00',
+			'palette' => $palette,
+		),
+		'article_bg' => array(
+			'label'   => __( 'Article Background'),
+			'default' => 'rgba( 20, 20, 20, 0.8 )',
+			'palette' => $palette,
+		),
+		'footer_bg' => array(
+			'label'   => __( 'Footer Background'),
+			'default' => '#00CC77',
+			'palette' => $palette,
+		),*/
+	);
+
+	/**
+	 * Set up the array of standard control data.
+	 *
+	 * This could also have an active_callback, a sanitize_callback, etc.
+	 */
+	$bg_colors_control_data = array(
+		'label'       => __( 'Theme Color palette' ),
+		'description' => __( 'The class to use the colors are in the format of <code>`block_name-type_name`</code>.\r\n\r\n <strong>type_name</strong> is the element\s part.\r\n  <strong>type_name</strong> is the element\s part.  Values supported are `bk` for background amd `text` for text. \r\n\r\nThis means for the &quot;Secoundary Accent Color&quot; color for the text block would be <code>secoundary-accent-text</code>' ),
+		'section'     => 'static_background',
+	);
+
+	/**
+	 * Use the helper function to register the group of settings and associate them with
+	 * a single Multi Color Picker control.
+	 */
+	components_register_color_group(
+		$wp_customize,
+		'spine_options',
+		$bg_colors,
+		$bg_colors_control_data,
+		$palette,
+		'postMessage'
+	);
+
 		// background color
-		$wp_customize->add_setting( 'spine_options[background_color]', array(
+		/*$wp_customize->add_setting( 'spine_options[background_color]', array(
 			'default' => false,
 			'capability' => 'edit_theme_options',
 			'type' => 'option',
 		) );
 		$wp_customize->add_control(
 			new WP_Customize_Color_Control(
-				$wp_customize,
-				'background_color',
-				array(
-				'label'      => __( 'Background Color' ),
-				'section'    => 'static_background',
-				'settings'   => 'spine_options[background_color]',
-				'description' => 'The class to use <code>background-color</code>',
+            $wp_customize,
+            'background_color',
+            array(
+            'label'      => __( 'Background Color' ),
+            'section'    => 'static_background',
+            'settings'   => 'spine_options[background_color]',
+            'description' => 'The class to use <code>background-color</code>',
 			) )
 		);
 
@@ -86,6 +195,8 @@ class Fais_Spine_Theme_Customizer {
 			) )
 		);
 
+
+
 		// secoundary_accent
 		$wp_customize->add_setting( 'spine_options[secoundary_accent_color]', array(
 			'default' => false,
@@ -103,6 +214,8 @@ class Fais_Spine_Theme_Customizer {
 				'description' => 'The primary color will alter areas like the top borders. The classes to use:<br/><b>Background</b> : <code>secoundary-accent-bk</code> <br/><b>Text</b> : <code>secoundary-accent-text</code>',
 			) )
 		);
+
+
 
 		// header_color
 		$wp_customize->add_setting( 'spine_options[header_color]', array(
@@ -136,6 +249,13 @@ class Fais_Spine_Theme_Customizer {
 				'settings'   => 'spine_options[header_text_color]',
 			) )
 		);
+
+
+
+
+
+
+		*/
 
 		$wp_customize->add_section( 'static_background', array(
 			'title' => __( 'FAIS theme settings', 'static_background' ),
@@ -215,3 +335,79 @@ class Fais_Spine_Theme_Customizer {
 	}
 }
 new Fais_Spine_Theme_Customizer();
+
+
+
+
+/**
+ * Helper function for registering a group of color settings.
+ *
+ * @param  Object  $wp_customize      The main Customizer object.
+ * @param  String  $option_name       The shared option name to use for the settings.
+ * @param  Array   $color_settings    The array of color settings data.
+ * @param  Array   $control_data      The data to pass to the control.
+ * @param  Array   $fallback_palette  An array of fallback palette colors to use if a palette is not included in $color_settings. (optional)
+ * @param  String  $transport         The transport method for the setting group.
+ */
+function components_register_color_group( $wp_customize, $option_name, $color_settings = array(), $control_data = array(), $fallback_palette = 'true', $transport = 'refresh' ) {
+
+	/**
+	 * Loop over the colors array and register each setting while also building
+	 * the color_settings and color_data arrays that we'll send to the control.
+	 */
+	foreach ( $color_settings as $setting_name => $setting_data ) {
+
+		// For this example we'll store all of our colors in a single settings array,
+		// which requires using the setting type "option". We could also use the
+		// setting type "theme_mod" by giving each setting its own unique option key.
+		$color_setting_id = $option_name . "[{$setting_name}]";
+
+		// Make default, palette, and show_opacity optional by providing fallbacks here.
+		$setting_data['default'] = ( isset( $setting_data['default'] ) ) ? $setting_data['default'] : '#000000';
+		$setting_data['palette'] = ( isset( $setting_data['palette'] ) ) ? $setting_data['palette'] : $fallback_palette;
+		$setting_data['show_opacity'] = ( isset( $setting_data['show_opacity'] ) ) ? $setting_data['show_opacity'] : 'true';
+
+		// Register the current setting.
+		// This still needs a proper sanitize_callback.
+		$wp_customize->add_setting(
+			$color_setting_id,
+			array(
+				'default'    => $setting_data['default'],
+				'type'       => 'option',
+				'capability' => 'edit_theme_options', // Modify this as needed.
+				'transport'  => $transport, // postMessage or refresh
+			)
+		);
+
+		// Build the simple array that contains only the color setting names.
+		// We'll pass this as the "settings" value to our control.
+		$settings[] = $color_setting_id;
+
+		// Build the more advanced color_data array that contains all the extra information
+		// we need for each color setting. We'll pass this to our control.
+		$color_data[ $color_setting_id ] = array(
+			'label'        => $setting_data['label'],
+			'default'      => $setting_data['default'],
+			'show_opacity' => $setting_data['show_opacity'],
+			'palette'      => $setting_data['palette'],
+		);
+	}
+
+	/**
+	 * Add our arrays to $control_data
+	 */
+	$control_data['settings'] = $settings;
+	$control_data['color_data'] = $color_data;
+
+	/**
+	 * Register the Multi Color Control.
+	 */
+	$wp_customize->add_control(
+		new Customize_Multi_Color_Control(
+			$wp_customize,
+			$option_name,
+			$control_data
+		)
+	);
+
+}
