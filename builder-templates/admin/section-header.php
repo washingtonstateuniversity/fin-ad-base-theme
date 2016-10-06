06abbdd0ff3dea7110d49bb7224c379f7b904944
Jeremy Bass <jeremybass@cableone.net>
2016-10-06 13:42:22 -0700

    make sure that we can see the setting for active when not

--- builder-templates/admin/section-header.php
+++ builder-templates/admin/section-header.php
@@ -2,7 +2,7 @@
 global $ttfmake_section_data, $ttfmake_is_js_template;
 ?>
 <style>
-.ttfmake-section.active-false{
+.ttfmake-section.active-false .ttfmake-section-header{
     position: relative;
     opacity: 0.6;
     border: .1rem dashed red;
