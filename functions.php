d0cc1fc3e75bd076ca59e8945be35e5c85838414
Jeremy Bass <jeremybass@cableone.net>
2016-10-21 15:06:30 -0700

    child style.css needs to dev most

--- functions.php
+++ functions.php
@@ -67,12 +67,12 @@ function fais_customizer_enqueue_scripts() {
 
 	$is_dev_mode = fais_spine_get_option( 'is_dev_mode', 'false' ); // yeah wil come base to case correctly, in rush ``/ lol
 	if( "true" === $is_dev_mode ){
-		wp_enqueue_style( 'fais_spine-theme',  'https://webcore.fais.wsu.edu/resources/central_FnA_theme/dev/wordpress/fin-ad-base-theme/style.css', array( 'wsu-spine' ), spine_get_script_version() );
 		$flex_dev = 'dev/';
 	}else{
-		wp_enqueue_style( 'fais_spine-theme',       get_template_directory_uri()   . '/style.css', array( 'wsu-spine' ), spine_get_script_version() );
 		$flex_dev = is_development() ? 'dev/' : '';
 	}
+
+	wp_enqueue_style( 'fais_spine-theme',       get_template_directory_uri()   . '/style.css', array( 'wsu-spine' ), spine_get_script_version() );
 
 	if ( 'skeletal' !== spine_get_option( 'theme_style' ) ) {
 		wp_enqueue_style( 'fais_spine-theme-extra', get_template_directory_uri()   . '/styles/' . spine_get_option( 'theme_style' ) . '.css', array( 'fais_spine-theme' ), spine_get_script_version() );
@@ -102,7 +102,12 @@ function fais_customizer_enqueue_scripts() {
 	wp_enqueue_style( 'flexwork-typography', 'https://webcore.fais.wsu.edu/resources/flexwork/'.$flex_dev .'extra/flexwork-typography.css', array( 'flexwork-base' ), spine_get_script_version() );
 	wp_enqueue_style( 'flexwork-ui', 'https://webcore.fais.wsu.edu/resources/flexwork/'.$flex_dev .'extra/flexwork-ui.css', array( 'flexwork-typography' ), spine_get_script_version() );
 
-	wp_enqueue_style( 'fais_spine-theme-child', get_stylesheet_directory_uri() . '/' . $child_stylesheet, array( 'flexwork-typography' ), spine_get_script_version() );
+
+	if( "true" === $is_dev_mode ){
+		wp_enqueue_style( 'fais_spine-theme',  'https://webcore.fais.wsu.edu/resources/central_FnA_theme/dev/wordpress/fin-ad-base-theme/style.css', array( 'flexwork-typography' ), spine_get_script_version() );
+	}else{
+		wp_enqueue_style( 'fais_spine-theme-child', get_stylesheet_directory_uri() . '/' . $child_stylesheet, array( 'flexwork-typography' ), spine_get_script_version() );
+	}
 
 	wp_enqueue_script( 'tether', 'https://webcore.fais.wsu.edu/resources/flexwork/'.$flex_dev .'extra/tether.min.js', array( 'jquery' ), spine_get_script_version(), true );
 	wp_enqueue_script( 'drop', 'https://webcore.fais.wsu.edu/resources/flexwork/'.$flex_dev .'extra/drop.min.js', array( 'tether' ), spine_get_script_version(), true );
