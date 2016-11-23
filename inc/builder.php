22ccdf62e5e213d5b8fdcb720ed0a9d5ff8a4e1d
Jeremy Bass <jeremybass@cableone.net>
2016-11-22 22:44:33 -0800

    wrong array on header save pad

--- inc/builder.php
+++ inc/builder.php
@@ -485,7 +485,7 @@ class Fais_Spine_Builder_Custom
 		}
 
 		if ( isset( $data['section-flextype'] ) ) {
-			$section_classes = $clean_data['section-flextype'];
+			$section_classes = $data['section-flextype'];
 			$section_classes = str_replace( array( 'flex-row', 'items-start', 'pad-tight', 'pad-airy', 'gutter', 'pad-ends' ), '', $section_classes );
 			$clean_data['section-flextype'] = $this->clean_classes( $section_classes );
 		}
