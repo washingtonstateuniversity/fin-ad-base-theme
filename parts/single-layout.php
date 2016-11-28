ce99ceb51b091efb1cd4a3f28c4c0dce70f61f58
Jeremy Bass <jeremybass@cableone.net>
2016-11-28 10:40:59 -0800

    no site centric items

--- parts/single-layout.php
+++ parts/single-layout.php
@@ -14,22 +14,6 @@
 
 		<?php get_sidebar(); ?>
 
-		<h4>Disposition Key:</h4>
-		<ul>
-			<li><strong>ACT: </strong>Active</li>
-			<li><strong>CLO: </strong>Closed Case</li>
-			<li><strong>AM: </strong>Alarm Malfunction</li>
-			<li><strong>ECP: </strong>Cleared Adult Prosecution Declined</li>
-			<li><strong>CAA: </strong>Cleared Adult Arrest</li>
-			<li><strong>ECV: </strong>Cleared Adult Victim/Uncooperative</li>
-			<li><strong>CEE: </strong>Created in Error</li>
-			<li><strong>EE: </strong>Employee Error</li>
-			<li><strong>CJA: </strong>Cleared Juvenile Arrest</li>
-			<li><strong>TRP: </strong>Transferred to Property</li>
-			<li><strong>RSP: </strong>Request Submit to Prosecutor</li>
-			<li><strong>RSJ: </strong>Request Submit to Juvenile Services</li>
-		</ul>
-
 	</div><!--/column two-->
 
 </section>
